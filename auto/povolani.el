ae97517ac1ca77dea1dedf14f73c7e147921c7db
moschehaus <belankkamil@gmail.com>
2024-10-31 21:33:46 +0100

    prace na berserkerovi

---
+++ auto/povolani.el
@@ -0,0 +1,13 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "povolani"
+ (lambda ()
+   (LaTeX-add-labels
+    "chap:speciality"
+    "sec:povolani-fate-core"
+    "sec:specialni-dovednosti"
+    "sec:specialni-triky"
+    "sec:berserker"))
+ :latex)
+
