0eabad1f119468fae66ba629a32a3b62ac49875a
moschehaus <belankkamil@gmail.com>
2024-11-04 13:31:41 +0100

    finish triku, zmena struktury

--- auto/povolani.el
+++ auto/povolani.el
@@ -3,11 +3,31 @@
 (TeX-add-style-hook
  "povolani"
  (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (LaTeX-add-labels
     "chap:speciality"
     "sec:povolani-fate-core"
-    "sec:specialni-dovednosti"
-    "sec:specialni-triky"
-    "sec:berserker"))
+    "sec:pov-bez-svoleni"
+    "sec:pov-bojovnik"
+    "sec:pov-strelec"
+    "sec:pov-zlodej"
+    "sec:pov-hranicar"
+    "sec:pov-sicco"
+    "sec:pov-se-svolenim"
+    "sec:pov-carodej"
+    "sec:pov-alchymista"
+    "sec:pov-theurg"
+    "sec:pov-knez"
+    "sec:pov-druid"
+    "sec:berserker"
+    "sec:pov-netvorobijec"
+    "sec:pov-subotam"
+    "sec:pov-edenak"))
  :latex)
 
---
+++ chapters/auto/predmety.el
@@ -0,0 +1,12 @@
+;; -*- lexical-binding: t; -*-
+
+(TeX-add-style-hook
+ "predmety"
+ (lambda ()
+   (LaTeX-add-labels
+    "chap:magicke-spec-predmety"
+    "sec:zbrane-zbroje"
+    "sec:alchymisticke-predmety"
+    "sec:magicke-vybaveni"))
+ :latex)
+
